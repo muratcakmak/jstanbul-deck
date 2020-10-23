167461de78f2c2690ec4cd52f1524ffe5aef0c99
Oguzhan Cakmak <muratcakmak@users.noreply.github.com>
2020-10-23 14:22:34 -0700

    Fix build problem

--- src/App.js
+++ src/App.js
@@ -29,48 +29,20 @@ const theme = {
   },
 };
 
-// const theme = {
-//   size: {
-//     width: 1366,
-//     height: 768,
-//     maxCodePaneHeight: 200,
+// const transition = {
+//   from: {
+//     position: "fixed",
+//     transform: "translate(100%, 0%)",
 //   },
-//   colors: {
-//     primary: "#ebe5da",
-//     secondary: "#fc6986",
-//     tertiary: "#1e2852",
-//     quaternary: "#ffc951",
-//     quinary: "#8bddfd",
+//   enter: {
+//     position: "fixed",
+//     transform: "translate(0, 0%)",
 //   },
-//   fonts: {
-//     header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
-//     text: '"Helvetica Neue", Helvetica, Arial, sans-serif',
-//     monospace: '"Consolas", "Menlo", monospace',
+//   leave: {
+//     position: "fixed",
+//     transform: "translate(-100%, 0%)",
 //   },
-//   fontSizes: {
-//     h1: "72px",
-//     h2: "64px",
-//     h3: "56px",
-//     text: "44px",
-//     monospace: "20px",
-//   },
-//   space: [16, 24, 32],
 // };
-
-const transition = {
-  from: {
-    position: "fixed",
-    transform: "translate(100%, 0%)",
-  },
-  enter: {
-    position: "fixed",
-    transform: "translate(0, 0%)",
-  },
-  leave: {
-    position: "fixed",
-    transform: "translate(-100%, 0%)",
-  },
-};
 
 const template = ({ slideNumber, numberOfSlides }) => (
   <FlexBox
